0047e91ca96786fe88c359fc77de273bd2cdac58
Mehmet Kanpınar <kanpinar@hotmail.com>
2016-02-15 15:52:49 +0200

    .

---
+++ WebApplication4/API/HeroesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WebApplication4.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication4.API
+{
+    [Route("api/[controller]")]
+    public class HeroesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<DOTAHero> Get()
+        {
+            HeroManager HM = new HeroManager();
+            return HM.GetAll;
+        }
+
+        // GET api/values/7
+        [HttpGet("{id}")]
+        public DOTAHero Get(int id)
+        {
+            HeroManager HM = new HeroManager();
+            return HM.GetHeroByID(id);
+        }
+
+    }
+
+}
---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id = null)
+        {
+            if (id != null)
+                return View("~/Views/Home/Index_" + id + ".cshtml");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
