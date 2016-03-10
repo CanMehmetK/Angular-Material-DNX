257a620242a28c757b324bc5a1d922cd70e4457d
Mehmet Kanpınar <kanpinar@hotmail.com>
2016-03-10 15:53:34 +0200

    ...

---
+++ UI/AspNetMVC/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Write your Javascript code.
+var CrmApp = angular.module('CrmApp', ['ngMaterial', 'ngMessages', function () {
+    console.log('app Start ->1');
+
+}]).run(function ($rootScope, $mdSidenav) {
+    console.log('Bootstrapper runned ->2');
+    // Global Değişkenler
+    $rootScope.testString = ServerUserName;
+    $rootScope.menu = {};
+    //
+    $rootScope.toggleSidenav = function (menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+}).filter('nospace', function () {
+    return function (value) {
+        return (!value) ? '' : value.replace(/ /g, '');
+    };
+}).filter('humanizeDoc', function () {
+    return function (doc) {
+        if (!doc) return;
+        if (doc.type === 'directive') {
+            return doc.name.replace(/([A-Z])/g, function ($1) {
+                return '-' + $1.toLowerCase();
+            });
+        }
+        return doc.label || doc.name;
+    };
+});
+
--- WebApplication4/wwwroot/lib/material-design-variables/README.md
+++
@@ -1,9 +0,0 @@
-## Material Design Variables
-
-Google's Material Design colour variables
-
-Fetched from: https://www.google.com/design/spec/style/color.html#color-color-palette
-
-Currently available for:
-* [Less](http://lesscss.org/)
-* [Sass](http://sass-lang.com/)
